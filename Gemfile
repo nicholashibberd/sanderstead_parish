fb9e3cf8bc085ad4f8cba4c2430032e23dc5e44e
nicholashibberd <nicholashibberd@gmail.com>
2012-05-30 19:06:59 +0100

    fixing the notices and sermons nav items

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'rails', '3.1.0'
 gem 'mongoid'
 gem 'bson_ext'
 gem 'nested_form', :git => "git://github.com/ryanb/nested_form.git"
-gem "cms", :git => "git://github.com/nicholashibberd/cms.git", :ref => '62bfb8b701'
+gem "cms", :git => "git://github.com/nicholashibberd/cms.git", :ref => 'ed6844f3ea'
 #gem "cms", :path => "../cms"
 
 # Gems used only for assets and not required
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,15 @@
 module ApplicationHelper
   def logo
-    if @page && @page.group
-      link_to image_tag("/assets/#{@page.group.slug}-logo.png"), root_path
+    if @group
+    #if @page && @page.group
+      link_to image_tag("/assets/#{@group.slug}-logo.png"), root_path
     else
       link_to image_tag("parish-logo.png"), root_path
     end
   end
   
   def sub_logo
-    if @page && @page.group
+    if @group
       link_to image_tag("parish-logo-small.png"), root_path
     end
   end
