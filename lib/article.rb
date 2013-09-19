3c5fa3128a337ff8ada1319c511a494742f103fb
nicholashibberd <nicholashibberd@gmail.com>
2013-09-19 23:22:15 +0100

    updating Artice.by_church_and_month method to take church rather than group as argument

--- lib/article.rb
+++ lib/article.rb
@@ -1,13 +1,13 @@
 module Article
-  def self.by_church_and_month(group, month)
+  def self.by_church_and_month(church, month)
     sermons = self.all
-    sermons = sermons.by_group(group.id) if group
+    sermons = sermons.by_group(church.id) if church
     sermons = sermons.select {|a| a.date.strftime('%m-%Y') == month} if month
     sermons
   end
 
-  def self.months_with_articles(group)
-    articles = by_church_and_month(group, nil)
+  def self.months_with_articles(church)
+    articles = by_church_and_month(church, nil)
     dates = articles.map(&:date)
     dates.map {|date| date.to_date.at_beginning_of_month}.uniq    
   end
